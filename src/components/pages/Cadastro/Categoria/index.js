4733c99e97c335cd066f89aa7f9ce1665c796fd9
Rafael <rafaelmassaro9@gmail.com>
2020-07-28 11:54:51 -0300

    Add Form

--- src/components/pages/Cadastro/Categoria/index.js
+++ src/components/pages/Cadastro/Categoria/index.js
@@ -5,7 +5,21 @@ import { Link } from 'react-router-dom';
 function CadastroCategoria(){
     return (
       <PageDefault>
-        <h1>Cadastrar Categoria</h1>
+        <h1>Cadastro de Categoria</h1>
+
+        <form>
+
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+            />
+          </label>
+
+          <button>
+            Cadastrar
+          </button>
+        </form>
 
         <Link to="/">
           Ir para a home
